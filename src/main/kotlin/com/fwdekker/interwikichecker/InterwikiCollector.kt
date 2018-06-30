b195ba29536625f22b61c9c35077eb875cd7eae4
Felix Dekker <felix@fwdekker.com>
2018-06-30 22:57:25 +0200

    Implement and test InterwikiNetwork

---
+++ src/main/kotlin/com/fwdekker/interwikichecker/InterwikiCollector.kt
@@ -0,0 +1,98 @@
+package com.fwdekker.interwikichecker
+
+import java.util.LinkedList
+
+/**
+ * Collects interwiki-related information on pages.
+ *
+ * @property downloader the [PageDownloader] used to download pages with
+ */
+class InterwikiCollector(private val downloader: PageDownloader) {
+    /**
+     * Builds a network of interwiki links between pages, starting from [location].
+     *
+     * This method works by recursively following interwiki links until no more pages are linked to. Data on the
+     * relationships between pages is returned in the form of an [InterwikiNetwork].
+     *
+     * @param location the location from which to start following interwiki links
+     * @return a network of interwiki links between pages, starting from [location]
+     */
+    fun buildInterwikiNetwork(location: PageLocation): InterwikiNetwork {
+        val links = HashMap<PageLocation, List<InterwikiLink>>()
+
+        val queue = LinkedList<PageLocation>()
+        queue.add(location)
+
+        while (queue.isNotEmpty()) {
+            val nextPage = queue.pop() ?: continue
+            if (links.containsKey(nextPage)) continue
+
+            downloader.downloadPage(nextPage).getInterwikiLinks()
+                .also {
+                    links[nextPage] = it
+                    queue.addAll(it.map { it.target })
+                }
+        }
+
+        return InterwikiNetwork(links)
+    }
+}
+
+/**
+ * A network of interwiki links between pages.
+ *
+ * @property links a mapping from a page to the interwiki links found on that page
+ */
+data class InterwikiNetwork(val links: Map<PageLocation, List<InterwikiLink>>) {
+    /**
+     * Returns the [PageLocation]s of all pages with language [language].
+     *
+     * @param language the language for which to return the [PageLocation]s
+     * @return the [PageLocation]s of all pages with language [language]
+     */
+    fun getPagesInLanguage(language: String) = links.keys.filter { it.language == language }
+
+    /**
+     * Returns all [InterwikiLink]s originating from [language].
+     *
+     * If there are multiple pages in the same language in this [InterwikiNetwork], the links are combined into a
+     * single list.
+     *
+     * @param language the language from which the [InterwikiLink]s originate
+     * @return all [InterwikiLink]s originating from [language]
+     */
+    fun getLinksFrom(language: String) =
+        links
+            .filter { it.key.language == language }
+            .flatMap { it.value }
+
+    /**
+     * Returns all [InterwikiLink]s originating from [page].
+     *
+     * @param page the page from which the [InterwikiLink]s originate
+     * @return all [InterwikiLink]s originating from [page]
+     */
+    fun getLinksFrom(page: PageLocation) = links[page]
+
+    /**
+     * Returns all [InterwikiLink]s that link to [targetLanguage].
+     *
+     * @param targetLanguage the language to which the [InterwikiLink]s link
+     * @return all [InterwikiLink]s that link to [targetLanguage]
+     */
+    fun getLinksTo(targetLanguage: String) =
+        links
+            .flatMap { it.value }
+            .filter { it.target.language == targetLanguage }
+
+    /**
+     * Returns all [InterwikiLink]s that link to [targetPage].
+     *
+     * @param targetPage the page to which the [InterwikiLink]s link
+     * @return all [InterwikiLink]s that link to [targetPage]
+     */
+    fun getLinksTo(targetPage: PageLocation) =
+        links
+            .flatMap { it.value }
+            .filter { it.target == targetPage }
+}
---
+++ src/test/kotlin/com/fwdekker/interwikichecker/InterwikiNetworkTest.kt
@@ -0,0 +1,67 @@
+package com.fwdekker.interwikichecker
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import java.lang.Math.random
+
+/**
+ * Unit tests for [InterwikiNetwork].
+ */
+internal object InterwikiNetworkTest : Spek({
+    fun randomLocation(language: String) = PageLocation(language, random().toString())
+
+    // Test data
+    val locations = listOf(
+        randomLocation("lu"),
+        randomLocation("en"),
+        randomLocation("lu"),
+        randomLocation("se"),
+        randomLocation("ch"),
+        randomLocation("se")
+    )
+    val links = mapOf(
+        Pair(locations[0], listOf(InterwikiLink(locations[0], locations[1]))),
+        Pair(locations[1], listOf(InterwikiLink(locations[1], locations[4]))),
+        Pair(locations[2], listOf(InterwikiLink(locations[2], locations[3]))),
+        Pair(locations[3], listOf(InterwikiLink(locations[3], locations[5]))),
+        Pair(locations[4], listOf(InterwikiLink(locations[4], locations[2]))),
+        Pair(locations[5], listOf(InterwikiLink(locations[5], locations[4])))
+    )
+    val network = InterwikiNetwork(links)
+
+
+    describe("interwiki networks") {
+        it("returns a list of pages in a particular language") {
+            assertThat(network.getPagesInLanguage("lu"))
+                .containsExactlyInAnyOrder(locations[0], locations[2])
+        }
+
+        it("returns a list of all links originating from a given language") {
+            assertThat(network.getLinksFrom("se"))
+                .containsExactlyInAnyOrder(
+                    InterwikiLink(locations[3], locations[5]),
+                    InterwikiLink(locations[5], locations[4])
+                )
+        }
+
+        it("returns a list of all links originating from a given page") {
+            assertThat(network.getLinksFrom(locations[2]))
+                .containsExactlyInAnyOrder(InterwikiLink(locations[2], locations[3]))
+        }
+
+        it("returns a list of all links going to a given language") {
+            assertThat(network.getLinksTo("ch"))
+                .containsExactlyInAnyOrder(
+                    InterwikiLink(locations[1], locations[4]),
+                    InterwikiLink(locations[5], locations[4])
+                )
+        }
+
+        it("returns a list of all links going to a given page") {
+            assertThat(network.getLinksTo(locations[1]))
+                .containsExactlyInAnyOrder(InterwikiLink(locations[0], locations[1]))
+        }
+    }
+})
