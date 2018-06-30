4327b1c178d61aa2a7458f883a20ea5fcc995564
Felix Dekker <felix@fwdekker.com>
2018-06-30 22:07:55 +0200

    Implement and test reading of pages

---
+++ src/main/kotlin/com/fwdekker/interwikichecker/Page.kt
@@ -0,0 +1,57 @@
+package com.fwdekker.interwikichecker
+
+import net.sourceforge.jwbf.core.contentRep.Article
+
+/**
+ * Represents a page on a wiki.
+ *
+ * @property location the location of this page
+ * @property article the [Article]
+ */
+class Page(private val location: PageLocation, private val article: Article) {
+    /**
+     * Returns a map of all interwiki links on this page as a map from the language to the page.
+     *
+     * @return a map of all interwiki links on this page as a map from the language to the page
+     */
+    fun getInterwikiLinks() =
+        extractInterwikiLinks(article.text)
+            .map { InterwikiLink(location, PageLocation(it.removeSurrounding("[[", "]]"))) }
+            .toList()
+
+    /**
+     * Returns all interwiki links that occur in [contents].
+     *
+     * An interwiki link is identified as a link of which the namespace contains exactly two characters. For
+     * example, `[[en:Page]]` and `[[uk:Help:Page]]` are interwiki links, but `[[Page]]` and `[[Category:Cat]]` are
+     * not.
+     *
+     * @param contents the contents to find interwiki links in
+     * @return all interwiki links that occur in [contents]
+     */
+    private fun extractInterwikiLinks(contents: String) =
+        Regex("\\[\\[.{2}:.*]]").findAll(contents).map { it.value }
+}
+
+/**
+ * The location of a page as its language and name.
+ *
+ * @param language the language of the page, such as "en" or "de"
+ * @param pageName the full name of the page, such as "Page" or "User:Foo"
+ */
+data class PageLocation(val language: String, val pageName: String) {
+    /**
+     * Creates a description of the location of a page from the link to that page including the language specifier.
+     *
+     * @param fullLink the location to a page including the language specifier, such as "en:Page" or "de:User:Foo"
+     */
+    constructor(fullLink: String) : this(fullLink.substringBefore(":"), fullLink.substringAfter(":"))
+}
+
+/**
+ * A link from one page to another.
+ *
+ * @param origin the page on which the link was found
+ * @param target the page to which the link links
+ */
+data class InterwikiLink(val origin: PageLocation, val target: PageLocation)
---
+++ src/test/kotlin/com/fwdekker/interwikichecker/PageTest.kt
@@ -0,0 +1,75 @@
+package com.fwdekker.interwikichecker
+
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import net.sourceforge.jwbf.core.contentRep.Article
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+/**
+ * Unit tests for [Page].
+ */
+internal object PageTest : Spek({
+    describe("extracting interwiki links") {
+        val location = PageLocation("language", "page name")
+
+        fun mockArticle(contents: String) = mock<Article> { on { it.text } doReturn contents }
+
+        fun createPage(contents: String) = Page(location, mockArticle(contents))
+
+        context("bad-weather cases") {
+            it("does not find any interwiki links in an empty document") {
+                assertThat(createPage("").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not find any interwiki links in a document without any links") {
+                assertThat(createPage("Ride Picking Avolate").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not think a regular link is an interwiki link") {
+                assertThat(createPage("[[Comptrol]]").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not think a two-letter link is an interwiki link") {
+                assertThat(createPage("[[it]]").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not think that a namespace identifies an interwiki link") {
+                assertThat(createPage("[[Beer:Hoss]]").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not think that a nested two-letter namespace identifies an interwiki link") {
+                assertThat(createPage("[[Relend:ty:Gemot]]").getInterwikiLinks()).isEmpty()
+            }
+
+            it("does not think that an interwiki link without square braces is an interwiki link") {
+                assertThat(createPage("en:Untripe").getInterwikiLinks()).isEmpty()
+            }
+        }
+
+        context("good-weather cases") {
+            it("finds a regular interwiki link") {
+                assertThat(createPage("[[kG:Sandyish]]").getInterwikiLinks())
+                    .containsExactly(InterwikiLink(location, PageLocation("kG", "Sandyish")))
+            }
+
+            it("finds a regular interwiki link that contains a namespace") {
+                assertThat(createPage("[[fr:Yachters:Martite]]").getInterwikiLinks())
+                    .containsExactly(InterwikiLink(location, PageLocation("fr", "Yachters:Martite")))
+            }
+
+            it("finds a regular interwiki link that contains a two-letter namespace") {
+                assertThat(createPage("[[yu:di:Nomina]]").getInterwikiLinks())
+                    .containsExactly(InterwikiLink(location, PageLocation("yu", "di:Nomina")))
+            }
+
+            it("finds an interwiki link even if the page name is empty") {
+                assertThat(createPage("[[aa:]]").getInterwikiLinks())
+                    .containsExactly(InterwikiLink(location, PageLocation("aa", "")))
+            }
+        }
+    }
+})
