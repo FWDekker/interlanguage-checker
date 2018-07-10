03046f87feeecbcf3db68b6094dc95c7d89531b1
Felix Dekker <felix@fwdekker.com>
2018-07-10 19:21:02 +0200

    Migrate to JavaFX

--- src/main/kotlin/com/fwdekker/interwikichecker/SimpleTableGui.kt
+++ src/main/kotlin/com/fwdekker/interwikichecker/SimpleTableGui.kt
@@ -1,73 +1,85 @@
 package com.fwdekker.interwikichecker
 
-import java.awt.BorderLayout
+import javafx.application.Application
+import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.event.ActionEvent
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.scene.control.TextField
+import javafx.scene.layout.BorderPane
+import javafx.stage.Stage
 import java.net.URL
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JPanel
-import javax.swing.JScrollPane
-import javax.swing.JTable
-import javax.swing.JTextField
 
-class SimpleTableGui(downloader: PageDownloader) {
-    private val collector = InterwikiCollector(downloader)
+class TableApplication : Application() {
+    private val collector: InterwikiCollector
 
-    private val pageSelection = JTextField("Rolling pin (Fallout 3)")
-    private val pageSelectionSubmit = JButton("Load")
-    private var table = JTable()
+    private val pageSelection = TextField()
+    private val pageSelectionSubmit = Button("Load")
+    private val table = TableView<ObservableList<PageLocation>>()
+
 
     init {
-        val frame = JFrame()
-        val panel = JPanel(BorderLayout())
-        frame.add(panel)
+        val downloader = PageDownloader(MediaWikiBotFactory(), createFalloutInterwikiMap())
+        collector = InterwikiCollector(downloader)
+    }
 
-        val controlPanel = JPanel()
-        panel.add(controlPanel, BorderLayout.NORTH)
-        controlPanel.add(pageSelection)
-        controlPanel.add(pageSelectionSubmit)
-        frame.rootPane.defaultButton = pageSelectionSubmit
 
-        pageSelectionSubmit.addActionListener {
-            table.model = createTableFor(PageLocation("en", pageSelection.text)).model
+    override fun start(stage: Stage) {
+        pageSelectionSubmit.setOnAction(::onPageSelectionSubmit)
+
+        val pane = BorderPane()
+        pane.top = pageSelection
+        pane.left = pageSelectionSubmit
+        pane.center = table
+
+        stage.scene = Scene(pane, 300.0, 500.0)
+        stage.show()
+    }
+
+    private fun onPageSelectionSubmit(event: ActionEvent) {
+        val network = collector.buildInterwikiNetwork(PageLocation("en", pageSelection.text))
+
+        table.items.setAll(network.toObservableTable())
+        table.columns.setAll(
+            network.pages
+                .sortedBy { it.toString() }
+                .mapIndexed { i, location ->
+                    TableColumn<ObservableList<PageLocation>, PageLocation>(location.toString())
+                        .apply {
+                            setCellValueFactory { data -> SimpleObjectProperty<PageLocation>(data.value[i]) }
+                        }
+                }
+        )
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(TableApplication::class.java)
         }
 
-        table = JTable()
-        val scrollPane = JScrollPane(table)
-
-        panel.add(scrollPane, BorderLayout.CENTER)
-        frame.pack()
-        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        frame.isVisible = true
-    }
-
-
-    private fun createTableFor(page: PageLocation): JTable {
-        val network = collector.buildInterwikiNetwork(page)
-        return JTable(network.toTable(), network.toHeaders())
+        fun createFalloutInterwikiMap() =
+            MediaWikiBotFactory().createMediaWikiBot(URL("https://fallout.wikia.com/"))
+                .siteinfo.interwikis
+                .filter { it.value.contains("fallout.wikia.com") }
+                .map { Pair(it.key, it.value.replace("/wiki", "")) }
+                .toMap()
     }
 }
 
-fun InterwikiNetwork.toHeaders() = pages.sortedBy { it.toString() }.toTypedArray()
-
-fun InterwikiNetwork.toTable() =
-    pages.sortedBy(PageLocation::toString).map { target ->
-        pages.sortedBy(PageLocation::toString).map { origin ->
-            when {
-                InterwikiLink(origin, target) in getLinksFrom(origin) -> target.toString()
-                origin == target -> "X"
-                else -> ""
-            }
-        }.toTypedArray()
-    }.toTypedArray()
-
-
-fun main(args: Array<String>) {
-    SimpleTableGui(PageDownloader(MediaWikiBotFactory(), createFalloutInterwikiMap()))
-}
-
-fun createFalloutInterwikiMap() =
-    MediaWikiBotFactory().createMediaWikiBot(URL("https://fallout.wikia.com/"))
-        .siteinfo.interwikis
-        .filter { it.value.contains("fallout.wikia.com") }
-        .map { Pair(it.key, it.value.replace("/wiki", "")) }
-        .toMap()
+private fun InterwikiNetwork.toObservableTable() =
+    FXCollections.observableList(
+        pages.sortedBy(PageLocation::toString).map<PageLocation, ObservableList<PageLocation>?> { target ->
+            FXCollections.observableList(
+                pages.sortedBy(PageLocation::toString).map { origin ->
+                    when {
+                        InterwikiLink(origin, target) in getLinksFrom(origin) -> target
+                        else -> null
+                    }
+                })
+        })
